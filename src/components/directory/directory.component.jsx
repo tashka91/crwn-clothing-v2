3308ddb430133c25f6f9f3129c2eef90aef33030
Yihua Zhang <yihuazhang2@gmail.com>
2022-03-11 21:49:05 -0500

    Update README.md

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from '../category-item/category-item.component';
+import './directory.styles.scss';
+
+const Directory = ({directories}) => {
+
+    return(
+        <div className='directory-container'>
+        { directories.map((directory)=>(
+        <CategoryItem category={directory} key={directory.id}/>
+        )) }
+    </div>
+    )
+
+}
+export default Directory;
